49a8dfd358452dfec9a04464edecc65fbe29af40
Max Lin <max60213@gmail.com>
2025-08-01 18:13:59 +0800

    feat: add GLSL shader support and TypeScript configuration

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.glsl$/,
+      type: 'asset/source',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
