d2a461e3a8e4c7228ad1870ff657238fffcbd9a0
Max Lin <max60213@gmail.com>
2025-08-01 18:14:34 +0800

    feat: update Gallery to use Next.js Image component with WebGL support

--- app/home/Gallery.tsx
+++ app/home/Gallery.tsx
@@ -4,9 +4,9 @@ const Gallery = () => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 justify-center justify-items-center items-center lg:grid-cols-3 gap-12 p-20">
       {
-        Array.from({ length: 10 }).map((_, index) => (
+        Array.from({ length: 9 }).map((_, index) => (
           <div className="flex flex-col gap-2 w-fit" key={index}>
-            <Image data-webgl-media src="https://picsum.photos/400/300" alt="test" width={400} height={300} />
+            <Image data-webgl-media src={`/images/${index + 1}.webp`} alt="test" width={400} height={300} />
             <h2>Image Title</h2>
             <p className="max-w-[400px] break-words">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, facilis.</p>
           </div>
